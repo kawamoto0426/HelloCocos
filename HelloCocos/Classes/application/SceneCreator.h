ef7e4f106fe052a0f468ae1362e1e865856d2c9a
kawamoto <tca.kawamoto@gmail.com>
2015-04-25 14:59:10 +0900

    cocosプロジェクトの作成

---
+++ HelloCocos/Classes/application/SceneCreator.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+class GameScene;
+class SceneCreator {
+
+	SceneCreator();
+
+public:
+
+	enum class SceneID {
+
+		HELLO_WORLD,
+		LIFE_GAME,
+		SHOOTING,
+		KAWAMOTO,
+
+#ifndef NDEBUG
+		DEBUG
+#endif
+
+	};
+
+public:
+
+	static GameScene *create( SceneID scene );
+
+};
+
+typedef SceneCreator::SceneID SceneID;
+inline GameScene *CreateScene( SceneID scene )
+{
+	return SceneCreator::create( scene );
+}
